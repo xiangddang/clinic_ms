e261690644cd4b37d3c3a9c24f069d68525076fa
Frances <xiang.yany@northeastern.edu>
2023-12-04 20:37:40 -0500

    task split

--- database/clinic_schema.sql
+++ database/clinic_schema.sql
@@ -239,7 +239,16 @@ END $$
 
 DELIMITER ;
 
--- 根据employee id获得员工的信息
+-- get information of employee by employee id
+delimiter $$
+
+create procedure get_employee_info(in employee_id)
+begin
+    select name, date_of_birth, phone, street, city, state, zipcode, biological_sex, spe_name 
+    from Employee where emp_id = employee_id
+    join specialty on specialty.spe_id = Employee.spe_id;
+end
+delimiter ;
 
 -- 员工离职后将其status设置为inactive，user account删除，但是不删除medical records，appointment，prescription等记录，
 -- 删除未来的appointment，删除doctor-nurse pair
@@ -303,22 +312,6 @@ begin
 end $$
 
 delimiter ;
-
--- manager可以管理doctor nurse pair，可以为doctor分配nurse，也可以取消doctor的nurse， 只有manager可以调这个procedure
-
--- 返回patient的所有billing
-
--- 给manager返回所有的一段时间的billing，可以按照时间排序
-
--- 根据medical_records_no返回所有medical records，包括diagnosis和prescription，
-
--- 返回所有的disease，供前端选择
-
--- 返回所有的medication，供前端选择
-
--- 为patient的medical records添加diagnosis，只有doctor可以调用这个procedure
-
--- 为patient的medical records添加prescription，只有doctor可以调用这个procedure
 
 
     
---
+++ database/procedures.sql
@@ -0,0 +1,16 @@
+
+-- manager可以管理doctor nurse pair，可以为doctor分配nurse，也可以取消doctor的nurse， 只有manager可以调这个procedure
+
+-- 返回patient的所有billing
+
+-- 给manager返回所有的一段时间的billing，可以按照时间排序
+
+-- 根据medical_records_no返回所有medical records，包括diagnosis和prescription，
+
+-- 返回所有的disease，供前端选择
+
+-- 返回所有的medication，供前端选择
+
+-- 为patient的medical records添加diagnosis，只有doctor可以调用这个procedure
+
+-- 为patient的medical records添加prescription，只有doctor可以调用这个procedure
