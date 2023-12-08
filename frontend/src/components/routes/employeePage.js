ddc933c91ec15353f903037b79fa71ac3b88b423
Frances <xiang.yany@northeastern.edu>
2023-12-07 22:13:39 -0500

    schedule delete

--- frontend/src/components/routes/employeePage.js
+++ frontend/src/components/routes/employeePage.js
@@ -2,7 +2,6 @@ import { Routes, Route } from "react-router-dom";
 import Employee from "../employee/employee_home.js";
 import EmployeeProfile from '../employee/employee_profile';
 import EditProfile from '../employee/emp_edit_profile.js';
-import CheckSchedule from "../employee/check_schedule.js";
 import CheckMedicalRecord from "../employee/check_medical_by_pid.js";
 
 function EmployeePage() {
@@ -12,7 +11,6 @@ function EmployeePage() {
         <Route path='/:username' element={<Employee />} />
         <Route path="/profile/:username/:employeeId" element={<EmployeeProfile />} />
         <Route path="/profile/:username/:employeeId/edit" element={<EditProfile />} />
-        <Route path='/check_schedule/:employeeId' element={<CheckSchedule />} />
         <Route path='/check_medical_rec/:patientId/:doctorId' element={<CheckMedicalRecord />} />
       </Routes>
     </div>
